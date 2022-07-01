1d70a74ae1ea7eb106c5f19e2b071317f5d223d2
pnsudhanva <pnsudhanva@gmail.com>
2022-07-01 09:46:18 -0700

    Modified the code to display day info

--- show_time.py
+++ show_time.py
@@ -1,4 +1,10 @@
 import time
+from datetime import date
+from datetime import datetime
 Time=time.localtime()
 current=time.strftime("%H:%M:%S", Time)
-print("The current time is: ", current) 
+print("The current time is: ", current)
+Date = date.today()
+print("Today's date:", Date)
+print("The current day is: ",datetime.today().strftime('%A'))
+
