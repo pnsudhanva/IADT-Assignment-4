93e06067a5ce2e43dfb783583dc4288d553bb957
pnsudhanva <pnsudhanva@gmail.com>
2022-06-30 19:33:19 -0700

    modified the code to show date

--- show_time.py
+++ show_time.py
@@ -1,4 +1,7 @@
 import time
+from datetime import date
 Time=time.localtime()
 current=time.strftime("%H:%M:%S", Time)
-print("The current time is: ", current) 
+print("The current time is: ", current)
+Date = date.today()
+print("Today's date:", Date)
